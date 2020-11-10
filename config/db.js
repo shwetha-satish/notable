86aefe1e107f5477abd92cd55b2c6b54d298057a
Shwetha Satish <shwetha.satish04@gmail.com>
2020-11-10 11:47:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : "mongodb+srv://xyz:qwerty12345@cluster0.cbyjr.mongodb.net/db_test_1?retryWrites=true&w=majority"
+    url : "mongodb+srv://test1:test1@cluster0.cbyjr.mongodb.net/db_test_2?retryWrites=true&w=majority"
 }
